5f0eafdba5dacf0e2a3daf8ac693d30090725596
sanuksd <100213849+sanuksd@users.noreply.github.com>
2025-02-23 13:37:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,6 @@ const firebaseConfig = {
     storageBucket: "coupenredeem.firebasestorage.app",
     messagingSenderId: "39525609057",
     appId: "1:39525609057:web:1c027d1f9b59808411c420",
-    measurementId: "G-S5XLQ75YFE"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
